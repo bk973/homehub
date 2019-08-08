cc8db6c3db8c1128a2eab1a9056a6e3bfdb1a2c8
BenKalungi <glukooya@gmail.com>
2019-08-08 17:10:06 +0300

    removed custom styles from sidenav in dashboard, now relying on bulma

--- src/layouts/dashboardLayout/components/sideNav/sideNav.js
+++ src/layouts/dashboardLayout/components/sideNav/sideNav.js
@@ -4,55 +4,48 @@ import './sideNav.scss'
 
 export default function sideNav() {
     return (
-        <div className="">
-            <div>
-                <ul className="">
+        <aside className="menu">
+                <ul className="menu-list">
                 <li>
-                    <Link to="/dashboard" className="sidenav-link">
+                    <Link to="/dashboard">
                         <span className="icon is-medium"><i class="fas fa-home"></i></span>
                         <span>Overview</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/dashboard/pending" className="sidenav-link">
+                    <Link to="/dashboard/pending">
                         <span className="icon is-medium"><i class="fas fa-exclamation-circle"></i></span>
                         <span>Pending requests</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/dashboard/approved" className="sidenav-link">
+                    <Link to="/dashboard/approved">
                         <span className="icon is-medium"><i class="fas fa-check-circle"></i></span>
                         <span>Approved requests</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/dashboard/sttings" className="sidenav-link">
+                    <Link to="/dashboard/history">
+                        <span className="icon is-medium"><i class="fas fa-history"></i></span>
+                        <span>History</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/dashboard/sttings">
                         <span className="icon is-medium"><i class="fas fa-cog"></i></span>
                         <span>Settings</span>
                     </Link>
                 </li>
                 </ul>
-            </div>
-            <div>
-            <ul className="">
+            <p className="menu-label">Resources</p>
+            <ul className="menu-list">
                 <li>
-                    <Link to="/dashboard/history" className="sidenav-link">
-                        <span className="icon is-medium"><i class="fas fa-history"></i></span>
-                        <span>History</span>
-                    </Link>
-                </li>
-                </ul>
-            </div>
-            <div>
-            <ul className="">
-                <li>
-                    <Link to="/dashboard/feedback" className="sidenav-link">
+                    <Link to="/dashboard/feedback">
                         <span className="icon is-medium"><i class="fas fa-comment-alt"></i></span>
                         <span>Feedback</span>
                     </Link>
                 </li>
-            </ul>
-        </div>   
-    </div>  
+            </ul> 
+    </aside>  
     )
 }
