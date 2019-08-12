a8a4f9f489fad4e5dc14c77d7749b2d36b13b1e6
BenKalungi <glukooya@gmail.com>
2019-08-12 12:58:27 +0300

    added the footer

--- src/routes/home/components/homePage/components/footer/footer.js
+++ src/routes/home/components/homePage/components/footer/footer.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './footer.scss'
 
 function Footer() {
     return (
-      
+        <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>
+              <Link className="link" to="">Blog</Link>
+            </strong>
+            <strong>
+              <Link className="link" to="">About</Link>
+            </strong>
+            <strong>
+              <Link className="link" to="">FAQ</Link>
+            </strong>
+            <strong>
+              <Link className="link" to="">Get help</Link>
+            </strong>
+          </p>
+          <p>
+            <span className="icon is-large"><i className="fab fa-facebook fa-lg"></i></span>
+            <span className="icon is-large"><i className="fab fa-twitter fa-lg"></i></span>
+          </p>
+          <p>(c)2019. Homehub inc,</p>
+        </div>
+      </footer>
     )
 }
 
