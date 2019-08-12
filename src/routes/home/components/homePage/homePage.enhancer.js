da24b82c1a5dc86e28ea91d1e7f48b82cc3a9ad6
BenKalungi <glukooya@gmail.com>
2019-08-12 13:02:15 +0300

    added google signin method

--- src/routes/home/components/homePage/homePage.enhancer.js
+++ src/routes/home/components/homePage/homePage.enhancer.js
@@ -1,6 +1,19 @@
-import { compose, setDisplayName } from 'recompose'
-//import { UserIsNotAuthenticated} from '../../../../utils/router'
+import { compose, setDisplayName, withHandlers } from 'recompose'
+import { withFirebase } from 'react-redux-firebase'
+import { withRouter } from 'react-router-dom'
+import { UserIsNotAuthenticated} from '../../../../utils/router'
+
+
 export default compose(
-      setDisplayName('...'),
-     // UserIsNotAuthenticated,
+      setDisplayName('Enhanced homePage'),
+      withFirebase,
+      UserIsNotAuthenticated,
+      //add props.history
+      withRouter,
+      withHandlers({
+            googleLogin: ({firebase, history}) => () => {
+                         firebase.login({ provider: 'google', type: 'popup'})
+                         .catch(err=> console.log(err))
+            }
+      })
 )
