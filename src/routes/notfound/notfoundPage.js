c776e160591e383ca8585028be7967bf77cd142e
BenKalungi <glukooya@gmail.com>
2019-08-13 12:25:29 +0300

    finalized not found page

--- src/routes/notfound/notfoundPage.js
+++ src/routes/notfound/notfoundPage.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { ReactComponent as Notfoundsvg } from '../../static/not_found.svg'
+import { Link } from 'react-router-dom'
+import './notfound.scss'
 
 export default function notfoundPage() {
     return (
-        <div>
-            OOoops, not found!!!
-        </div>
+       <React.Fragment>
+           <div className="columns">
+               <div className="column"></div>
+               <div className="column wrapper">
+                   <Notfoundsvg width="400" height="200"/>
+                   <p className="title is-4">
+                       OOoops! Page not found
+                   </p>
+                   <Link to="/">Back to home</Link>
+               </div>
+               <div className="column"></div>
+           </div>
+       </React.Fragment>
     )
 }
