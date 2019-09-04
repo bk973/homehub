72983d30c8377bce3a987e5c8331d282d133e7ea
BenKalungi <glukooya@gmail.com>
2019-09-04 14:49:58 +0300

    added material ui design system with js styles

---
+++ src/layouts/dashboardLayout/dashboardLayout.styles.js
@@ -0,0 +1,31 @@
+const drawerWidth = 70;
+
+export default theme => ({
+       root: {
+           display: 'flex',
+       },
+       appbar: {
+           width: `calc(100% - ${drawerWidth}px)`,
+           marginLeft: drawerWidth,
+           backgroundColor: '#fff',
+           borderBottom: '2px solid #ccc',
+           boxShadow: '0 0 0'
+       },
+       drawer: {
+           width: drawerWidth,
+           flexShrink: 0,
+       },
+       drawerPaper: {
+           width: drawerWidth,
+           backgroundColor: theme.palette.primary.main
+      },
+      toolbar: theme.mixins.toolbar,
+      content: {
+          flexGrow: 1,
+          backgroundColor: theme.palette.background.default,
+          padding: theme.spacing(3),
+  },
+  spacer: {
+      flexGrow: 1
+  }
+})
