1aa45db1a6e014bb2f963708f58cc9d4a50827c8
BenKalungi <glukooya@gmail.com>
2019-09-04 14:46:14 +0300

    added material-ui theme

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export default {
+    palette: {
+      primary: {
+        main: '#2196f3'
+      },
+      background: {
+        default: '#fff'
+      }
+    },
+    // Enable typography v2: https://material-ui.com/style/typography/#migration-to-typography-v2
+    typography: {
+      useNextVariants: true
+    },
+    flexColumnCenter: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    },
+    flexRowCenter: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center'
+    }
+  }
+  
